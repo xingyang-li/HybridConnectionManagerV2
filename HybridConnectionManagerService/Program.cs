5511ab892a2e02b3fd45173976c3c0cbf8206f37
xingyangli1278 <xingyangli1278@gmail.com>
2025-03-13 09:53:07 -0700

    change log file to 1mb max

--- HybridConnectionManagerService/Program.cs
+++ HybridConnectionManagerService/Program.cs
@@ -11,7 +11,7 @@ namespace HybridConnectionManager.Service
             Util.SetupFileDependencies();
 
             Log.Logger = new LoggerConfiguration()
-            .WriteTo.File(Util.AppDataLogFileTemplate, rollingInterval: RollingInterval.Day, rollOnFileSizeLimit: true, fileSizeLimitBytes: 104857600, shared: true)
+            .WriteTo.File(Util.AppDataLogFileTemplate, rollingInterval: RollingInterval.Day, rollOnFileSizeLimit: true, fileSizeLimitBytes: 1048576 /* 1 MB max log file size */, shared: true)
             .CreateLogger();
 
             LogProvider.LogInfo("Starting up Hybrid Connection Manager V2 Service with saved connections.");
